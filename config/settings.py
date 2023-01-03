6ce284eae51298a6e64d44b5c1131ba43a3b0b6c
timosahenru <timosahenru@gmail.com>
2023-01-03 12:28:12 +0000

    removed whitenoise

--- config/settings.py
+++ config/settings.py
@@ -38,7 +38,6 @@ AUTH_USER_MODEL = 'app.Agent'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
